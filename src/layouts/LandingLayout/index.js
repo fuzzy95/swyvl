474b64ccc4268fc4cd8456561fdeebb07e2f3dd5
fuzzy95 <asif.eshaq@northsouth.edu>
2021-07-17 11:58:29 +0600

    added images

--- src/layouts/LandingLayout/index.js
+++ src/layouts/LandingLayout/index.js
@@ -1,28 +1,54 @@
-import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+import whiteLogo from '../../assets/images/whiteText.svg';
 import Footer from '../../components/Footer';
 import Navbar from '../../components/Navbar';
 
 const LandingLayout = ({ children }) => {
-  const [firstScreen, setFirstScreen] = useState(true);
+  // const [firstScreen, setFirstScreen] = useState(true);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setFirstScreen(false);
-    }, 5000);
-  }, []);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setFirstScreen(false);
+  //   }, 3000);
+  // }, []);
 
   return (
     <>
-      {/* {firstScreen ? (
-        <div
-          style={{ zIndex: '9999', overflow: 'hidden' }}
-          className='fixed h-screen w-screen bg-darlBlue overflow-hidden'
+      <motion.div
+        style={{ zIndex: '9999', overflow: 'hidden' }}
+        initial={{ opacity: 1 }}
+        animate={{ opacity: 0 }}
+        transition={{
+          delay: 7,
+        }}
+        className='fixed h-screen w-screen bg-darlBlue overflow-hidden flex justify-center items-center'
+      >
+        <motion.div
+          initial={{ x: 140 }}
+          animate={{
+            x: -10,
+          }}
+          transition={{
+            delay: 1,
+            default: { duration: 3 },
+          }}
+          className='flex justify-center items-center'
         >
-          <img src='' alt='' />
-        </div>
-      ) : (
-        <> </>
-      )} */}
+          <img className='w-14' src='/SYMBOL.svg' alt='' />
+        </motion.div>
+
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{
+            delay: 7,
+            default: { duration: 6 },
+          }}
+          className='flex justify-center items-center'
+        >
+          <img className='w-72' src={whiteLogo} alt='' />
+        </motion.div>
+      </motion.div>
       <Navbar />
       {children}
       <Footer />
